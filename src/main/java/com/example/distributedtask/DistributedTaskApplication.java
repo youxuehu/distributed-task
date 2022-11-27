0e579a79489480225d09b8e6290a7a0e75d3ea69
youxuehu <youxuehu@hotmail.com>
2022-11-27 15:09:29 +0800

    init

---
+++ src/main/java/com/example/distributedtask/DistributedTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributedtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/distributedtask/DistributedTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributedtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
